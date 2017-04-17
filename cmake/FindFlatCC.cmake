1d8d8b4f7c3f8669168fb9363f5a06dc33d40c45
Stephen Lane-Walsh <sdl.slane@gmail.com>
2017-04-16 22:27:30 -0400

    Fixed Windows FlatBuffer config

--- cmake/FindFlatCC.cmake
+++ cmake/FindFlatCC.cmake
@@ -27,45 +27,98 @@
 #   for the given flatbuffer schema files.
 #   Returns the header files in ${Name}_OUTPUTS
 
+set(FLATCC_SEARCH_PATHS
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local
+	/usr
+	/sw # Fink
+	/opt/local # DarwinPorts
+	/opt/csw # Blastwave
+	/opt
+	$ENV{VS140COMNTOOLS}../../VC # VS 2015
+	$ENV{VS120COMNTOOLS}../../VC # VS 2013
+	$ENV{VS110COMNTOOLS}../../VC # VS 2012
+	$ENV{VS100COMNTOOLS}../../VC # VS 2010
+)
+
 set(FLATCC_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-find_program(FLATCC_FLATC_EXECUTABLE NAMES flatcc)
-find_path(FLATCC_INCLUDE_DIR NAMES flatcc/flatcc.h)
-find_library(FLATCC_LIBRARY NAMES flatcc)
-find_library(FLATCC_RUNTIME_LIBRARY NAMES flatccrt)
+find_path(FLATCC_INCLUDE_DIR 
+	NAMES flatcc/flatcc.h
+	HINTS
+	PATH_SUFFIXES include
+	PATHS ${FLATCC_SEARCH_PATHS}
+)
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8) 
+	set(PATH_SUFFIXES lib64 lib/x64 lib/amd64 lib)
+else() 
+	set(PATH_SUFFIXES lib/x86 lib)
+endif() 
+
+find_library(FLATCC_LIBRARY 
+	NAMES flatcc
+	HINTS
+	PATH_SUFFIXES ${PATH_SUFFIXES}
+	PATHS ${FLATCC_SEARCH_PATHS}
+)
+find_library(FLATCC_RUNTIME_LIBRARY 
+	NAMES flatccrt
+	HINTS
+	PATH_SUFFIXES ${PATH_SUFFIXES}
+	PATHS ${FLATCC_SEARCH_PATHS}
+)
+
+if(CMAKE_SIZEOF_VOID_P EQUAL 8) 
+	set(PATH_SUFFIXES bin/amd64 bin)
+else() 
+	set(PATH_SUFFIXES bin)
+endif() 
+
+find_program(FLATCC_FLATC_EXECUTABLE 
+	NAMES flatcc
+	HINTS
+	PATH_SUFFIXES ${PATH_SUFFIXES}
+	PATHS ${FLATCC_SEARCH_PATHS}
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(flatcc
-    DEFAULT_MSG FLATCC_FLATC_EXECUTABLE FLATCC_INCLUDE_DIR FLATCC_LIBRARY FLATCC_RUNTIME_LIBRARY)
+	REQUIRED_VARS FLATCC_FLATC_EXECUTABLE FLATCC_INCLUDE_DIR FLATCC_LIBRARY FLATCC_RUNTIME_LIBRARY)
 
 if(FLATCC_FOUND)
-  function(FLATCC_GENERATE_C_HEADERS Name)
-    set(FLATCC_COMMON_READER "${CMAKE_CURRENT_BINARY_DIR}/flatbuffers_common_reader.h")
-    add_custom_command(OUTPUT ${FLATCC_COMMON_READER}
-      COMMAND ${FLATCC_FLATC_EXECUTABLE}
-      ARGS -c -o "${CMAKE_CURRENT_BINARY_DIR}/"
-      COMMENT "Building C common flatbuffer reader"
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+	function(FLATCC_GENERATE_C_HEADERS Name)
+		set(FLATCC_COMMON 
+			"${CMAKE_CURRENT_BINARY_DIR}/flatbuffers_common_reader.h" 
+			"${CMAKE_CURRENT_BINARY_DIR}/flatbuffers_common_builder.h"
+		)
+		add_custom_command(OUTPUT ${FLATCC_COMMON}
+			COMMAND ${FLATCC_FLATC_EXECUTABLE}
+			ARGS -cw -o "${CMAKE_CURRENT_BINARY_DIR}/"
+			COMMENT "Building C common flatbuffer reader"
+			WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-    set(FLATCC_OUTPUTS ${FLATCC_COMMON_READER})
-    foreach(FILE ${ARGN})
-      get_filename_component(FLATCC_OUTPUT ${FILE} NAME_WE)
-      set(FLATCC_OUTPUT
-        "${CMAKE_CURRENT_BINARY_DIR}/${FLATCC_OUTPUT}_reader.h")
-      list(APPEND FLATCC_OUTPUTS ${FLATCC_OUTPUT})
+		set(FLATCC_OUTPUTS ${FLATCC_COMMON})
+		foreach(FILE ${ARGN})
+			get_filename_component(FLATCC_OUTPUT ${FILE} NAME_WE)
+			set(FLATCC_OUTPUT
+				"${CMAKE_CURRENT_BINARY_DIR}/${FLATCC_OUTPUT}_reader.h"
+				"${CMAKE_CURRENT_BINARY_DIR}/${FLATCC_OUTPUT}_builder.h")
+			list(APPEND FLATCC_OUTPUTS ${FLATCC_OUTPUT})
 
-      add_custom_command(OUTPUT ${FLATCC_OUTPUT}
-        DEPENDS ${FILE}
-        COMMAND ${FLATCC_FLATC_EXECUTABLE}
-        ARGS -w -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
-        COMMENT "Building C header for ${FILE}"
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-    endforeach()
-    set(${Name}_OUTPUTS ${FLATCC_OUTPUTS} PARENT_SCOPE)
-  endfunction()
+			add_custom_command(OUTPUT ${FLATCC_OUTPUT}
+				DEPENDS ${FILE}
+				COMMAND ${FLATCC_FLATC_EXECUTABLE}
+				ARGS -w -o "${CMAKE_CURRENT_BINARY_DIR}/" ${FILE}
+				COMMENT "Building C header for ${FILE}"
+				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+		endforeach()
+		set(${Name}_OUTPUTS ${FLATCC_OUTPUTS} PARENT_SCOPE)
+	endfunction()
 
-  set(FLATCC_INCLUDE_DIRS ${FLATCC_INCLUDE_DIR})
-  include_directories(${CMAKE_BINARY_DIR})
+	set(FLATCC_INCLUDE_DIRS ${FLATCC_INCLUDE_DIR})
+	include_directories(${CMAKE_BINARY_DIR})
 else()
-  set(FLATCC_INCLUDE_DIR)
+	set(FLATCC_INCLUDE_DIR)
 endif()
--- setup.ps1
+++ setup.ps1
@@ -7,10 +7,11 @@ Paste into Powershell CLI and remove quotes to Run
 #>
 
 $TEMP_DIR = $env:TEMP
-$SDL2_VERSION = "2.0.5"
+$SDL2_VERSION 		= "2.0.5"
 $SDL2_IMAGE_VERSION = "2.0.1"
-$SDL2_TTF_VERSION = "2.0.14"
-$VC_DIR = "$($Env:VS140COMNTOOLS)\..\..\VC\"
+$SDL2_TTF_VERSION 	= "2.0.14"
+$FLATCC_VERSION 	= "0.4.1"
+$VC_DIR = "$($Env:VS140COMNTOOLS)..\..\VC\"
 
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 function Unzip
@@ -22,6 +23,7 @@ function Unzip
 
 Write-Host "Downloading SDL2"
 Invoke-WebRequest "https://www.libsdl.org/release/SDL2-devel-$($SDL2_VERSION)-VC.zip" -OutFile "$($TEMP_DIR)\SDL2-$($SDL2_VERSION).zip"
+Remove-Item "$($TEMP_DIR)\SDL2-$($SDL2_VERSION)" -Recurse -Force | Out-Null
 Unzip "$($TEMP_DIR)\SDL2-$($SDL2_VERSION).zip" "$($TEMP_DIR)\"
 
 Write-Host "Installing SDL2"
@@ -33,6 +35,7 @@ Copy-Item "$($TEMP_DIR)\SDL2-$($SDL2_VERSION)\lib\x64\*.dll" "$($VC_DIR)\bin\amd
 
 Write-Host "Downloading SDL2_image"
 Invoke-WebRequest "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-$($SDL2_IMAGE_VERSION)-VC.zip" -OutFile "$($TEMP_DIR)\SDL2_image-$($SDL2_IMAGE_VERSION).zip"
+Remove-Item "$($TEMP_DIR)\SDL2_image-$($SDL2_IMAGE_VERSION)" -Recurse -Force | Out-Null
 Unzip "$($TEMP_DIR)\SDL2_image-$($SDL2_IMAGE_VERSION).zip" "$($TEMP_DIR)\"
 
 Write-Host "Installing SDL2_image"
@@ -44,6 +47,7 @@ Copy-Item "$($TEMP_DIR)\SDL2_image-$($SDL2_IMAGE_VERSION)\lib\x64\*.dll" "$($VC_
 
 Write-Host "Downloading SDL2_ttf"
 Invoke-WebRequest "https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-$($SDL2_TTF_VERSION)-VC.zip" -OutFile "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION).zip"
+Remove-Item "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION)" -Recurse -Force | Out-Null
 Unzip "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION).zip" "$($TEMP_DIR)\"
 
 Write-Host "Installing SDL2_ttf"
@@ -52,3 +56,25 @@ Copy-Item "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION)\lib\x86\*.lib" "$($VC_DIR)
 Copy-Item "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION)\lib\x86\*.dll" "$($VC_DIR)\bin\" -Recurse -Force
 Copy-Item "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION)\lib\x64\*.lib" "$($VC_DIR)\lib\amd64\" -Recurse -Force
 Copy-Item "$($TEMP_DIR)\SDL2_ttf-$($SDL2_TTF_VERSION)\lib\x64\*.dll" "$($VC_DIR)\bin\amd64\" -Recurse -Force
+
+Write-Host "Cloning FlatCC"
+Remove-Item "$($TEMP_DIR)\flatcc" -Recurse -Force | Out-Null
+git clone --depth=1 --branch="v$($FLATCC_VERSION)" "https://github.com/dvidelabs/flatcc.git" "$($TEMP_DIR)\flatcc" | Out-Null
+New-Item -Type Directory "$($TEMP_DIR)\flatcc\build\x86" | Out-Null
+New-Item -Type Directory "$($TEMP_DIR)\flatcc\build\x64" | Out-Null
+
+Write-Host "Building FlatCC x86"
+cd "$($TEMP_DIR)\flatcc\build\x86"
+cmake -G "Visual Studio 14 2015" ..\.. | Out-Null
+cmake --build . --target --config Release | Out-Null
+Copy-Item "$($TEMP_DIR)\flatcc\include\*" "$($VC_DIR)\include\" -Recurse -Force
+Copy-Item "$($TEMP_DIR)\flatcc\lib\Release\*" "$($VC_DIR)\lib\" -Recurse -Force
+Copy-Item "$($TEMP_DIR)\flatcc\bin\Release\*" "$($VC_DIR)\bin\" -Recurse -Force
+
+Write-Host "Building FlatCC x64"
+cd "$($TEMP_DIR)\flatcc\build\x64"
+cmake -G "Visual Studio 14 2015 Win64" ..\.. | Out-Null
+cmake --build . --target --config Release | Out-Null
+Copy-Item "$($TEMP_DIR)\flatcc\include\*" "$($VC_DIR)\include\" -Recurse -Force
+Copy-Item "$($TEMP_DIR)\flatcc\lib\Release\*" "$($VC_DIR)\lib\amd64\" -Recurse -Force
+Copy-Item "$($TEMP_DIR)\flatcc\bin\Release\*" "$($VC_DIR)\bin\amd64\" -Recurse -Force
